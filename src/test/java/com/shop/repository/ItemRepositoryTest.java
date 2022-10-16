1fa1d61dbfc028b69aaaca0f380f24461dfcb96b
godot4027 <godot4027@nate.com>
2022-10-16 12:10:40 +0900

    깃허브 업로드 테스트

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestPropertySource(locations="classpath:application-test.properties")
 class ItemRepositoryTest {
 
-    @Autowired
+    @Autowired 
     ItemRepository itemRepository;
 
     @Test
